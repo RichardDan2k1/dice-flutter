805ea1f59e839f4f925066cd703673bfa60409de
RichardDan2k1 <ricdan2k1@gmail.com>
2023-10-24 16:00:43 +0530

    Initial commit

---
+++ diceflutter/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text('Dice'),
+        backgroundColor: Colors.red,
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatefulWidget {
+
+
+  @override
+  State<DicePage> createState() => _State();
+}
+
+class _State extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+
+void changeDice(){
+  setState(() {
+    leftDiceNumber=Random().nextInt(6)+1;
+    rightDiceNumber= Random().nextInt(6)+1;
+  });
+
+}
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(onPressed: () {
+              setState(() {
+                changeDice();
+              });
+            },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+
+          Expanded(
+            child:  TextButton(onPressed: () {
+             setState(() {
+              changeDice();
+             });
+            },
+                child: Image.asset('images/dice$rightDiceNumber.png')),
+          ),
+        ],
+      ),
+    );
+  }
+  }
+
